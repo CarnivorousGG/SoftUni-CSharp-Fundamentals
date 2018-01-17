9d7e296abfb28ec4c15f91e9b214f337a4aa6092
Konstantin Genov <k.genov@hotmail.com>
2018-01-17 20:49:40 +0200

    Solution to the first exercise

---
+++ Lists/01. RemoveNegativesAndReverse/01.RemoveNegativersAndReverse.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveNegativesAndReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            List<int> result = new List<int>();
+
+
+            foreach (var number in numbers)
+            {
+                if (number > 0)
+                {
+                    result.Add(number);
+                }
+            }
+
+            result.Reverse();
+
+            if (result.Count > 0)
+            {
+                Console.WriteLine(string.Join(" ", result));
+            }
+            else
+            {
+                Console.WriteLine("empty");
+            }
+            
+        }
+    }
+}
