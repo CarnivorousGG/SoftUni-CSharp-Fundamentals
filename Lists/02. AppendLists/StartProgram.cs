c883f9807b9ba44151c5dcb9b8b0159c85072e68
Konstantin Genov <k.genov@hotmail.com>
2018-01-20 18:13:01 +0200

    solution to the second exercise

---
+++ Lists/02. AppendLists/StartProgram.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.AppendLists
+{
+    class StartProgram
+    {
+        static void Main(string[] args)
+        {
+
+            string[] input = Console.ReadLine().Split('|').ToArray();
+            List<string> result = new List<string>();
+            Array.Reverse(input);
+
+            foreach (var token in input)
+            {
+                string[] final = token.Split(' ');
+
+                foreach (var number in final)
+                {
+                    if (number != " ")
+                    {
+                        result.Add(number);
+                    }
+
+                }
+            }
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
